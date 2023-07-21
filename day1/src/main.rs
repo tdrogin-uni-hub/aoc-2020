d8c257821555adbc2be12b702113bf9f465131fa
Tym Drogin <tim.drogin@gmail.com>
2023-07-21 01:42:53 +0100

    day1 - done

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use itertools::Itertools;
+
+fn main() -> anyhow::Result<()> {
+    let (a, b, c) = include_str!("input.txt")
+        .lines()
+        .map(str::parse::<i64>)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .tuple_combinations()
+        .find(|(a, b, c)| a + b + c == 2020)
+        .expect("no triple had a sum of 2020");
+
+    dbg!(a + b + c);
+    dbg!(a * b * c);
+
+    Ok(())
 }
